54f1165aa8140aa221799cf610413c1f0a89863a
Gur Dotan <gurdotan@gmail.com>
2014-03-30 20:59:53 +0300

    Add grunt build without dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,49 @@
 module.exports = function(grunt) {
 
-    var distFolder      = "./dist",
-        srcFolder       = "./src";
+  var distFolder = "./dist",
+    srcFolder = "./src";
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        requirejs : {
-            compile : {
-                options : {
-                    mainConfigFile  : srcFolder + '/main.js',
+  var requirejs = {
+    compile: {
+      options: {
+        mainConfigFile: srcFolder + '/main.js',
                     name            : "main",
                     out             : distFolder + "/main.js"
-                }
-            }
-        },
-        clean: [distFolder]
-    });
+      }
+    }
+  }
 
-    // Uncomment to prevent code minification
-    // grunt.config("requirejs.compile.options.optimize", "none");
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    requirejs: requirejs,
+    clean    : [distFolder]
+  });
 
+  // Uncomment to prevent code minification
+  // grunt.config("requirejs.compile.options.optimize", "none");
 
-    grunt.registerTask('default', ['clean', 'requirejs']);
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('build:noDeps', 'Build js-store non minified and without Backbone dependencies', function() {
+
+    // Adapt the requirejs config to output
+    // a module without the backbone dependencies
+    delete requirejs.compile.options.name;
+    delete requirejs.compile.options.out;
+    requirejs.compile.options.dir = distFolder;
+    requirejs.compile.options.optimize = 'none';
+    requirejs.compile.options.modules = [
+      {
+        name   : "main",
+        exclude: ["backbone"]
+      }
+    ];
+
+    grunt.config("requirejs", requirejs);
+    grunt.task.run('default');
+  });
+
+  grunt.registerTask('default', ['clean', 'requirejs']);
 };
